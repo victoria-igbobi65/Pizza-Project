b2040ff614b6346393ad8060f51f354619f39fed
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-12-11 17:58:34 +0100

    config files for application

--- config/config.js
+++ config/config.js
@@ -1,8 +1,14 @@
 require('dotenv').config()
 
 module.exports = {
-  mongoUrl: process.env.DB_URL,
-  port: process.env.PORT,
-  jwtSecret: process.env.JWTSECRET,
-};
+    mongoUrl: process.env.DB_URL,
+    port: process.env.PORT,
+    jwtSecret: process.env.JWTSECRET,
+    email_username: process.env.EMAIL_USERNAME,
+    email_password: process.env.EMAIL_PASSWORD,
+    email_host: process.env.EMAIL_HOST,
+    email_port: process.env.EMAIL_PORT,
+    email_from: process.env.EMAIL_FROM,
+    node_env: process.env.NODE_ENV
+}
 
