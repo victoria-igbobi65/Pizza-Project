981785dbc3bae40a8b28ccac2002ff676eef5072
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-01-01 23:11:34 +0100

    updated file

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,7 +2,8 @@ const userModel = require('../models/userModel')
 const AppError = require('../utils/appError')
 const catchAsync = require('../utils/catchAsync')
 const Token = require('../utils/token')
-const welcomeMail = require('../utils/Emails/welcome')
+// const welcomeMail = require('../utils/Emails/welcome')
+
 
 exports.signUp = catchAsync( async (req, res, next) => {
     const { firstName, lastName, email, phoneNumber, address, password } = req.body;
@@ -19,9 +20,20 @@ exports.signUp = catchAsync( async (req, res, next) => {
 
     /*returning new user*/
     newUser.password = undefined
-
-    /*send email*/
-    await welcomeMail({name: newUser.firstName, email: newUser.email})
+    //await welcomeMail({ name: newUser.firstName, email: newUser.email })
     Token.sendToken(newUser, 200, res)
 
+})
+
+/*Login controller*/
+exports.login = catchAsync( async( req, res, next ) => {
+    const {email, password} = req.body
+    if (!email || !password) return next(new AppError('Please provide email and password!', 400))
+
+    /*Check user by email */
+    const user = await userModel.findOne({ email })
+
+    /* return error message if no user with the email address was found */
+    if (!user || !( await user.correctPassword(password)) ) return next(new AppError('Incorrect email or password!', 400))
+    Token.sendToken(user, 200, res)
 })
