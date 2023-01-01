a37997f7336f55c3bce9f7e72a852c7596914169
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-01-01 23:13:06 +0100

    updated file

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,16 +1,21 @@
 const jwt = require('jsonwebtoken')
+const welcomeMail = require('../utils/Emails/welcome')
 require('dotenv').config()
 
 const signToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET)
 }
 
-exports.sendToken = (user, statusCode, res) => {
+exports.sendToken = async (user, statusCode, res) => {
     const token = signToken(user._id);
     const cookieOptions = {
         httpOnly: true,
     }
-    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+    if (process.env.NODE_ENV === 'production') {
+        cookieOptions.secure = true
+    }
+    
+    await welcomeMail({ name: user.firstName, email: user.email })
     res.cookie('jwt', token, cookieOptions );
 
 
